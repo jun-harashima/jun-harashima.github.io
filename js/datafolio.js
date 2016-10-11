5956766e243b939ae695c6bbe9f9d18060258915
Jun Harashima <jun-harashima@cookpad.com>
2016-10-11 21:51:29 +0900

    first commit

---
+++ js/datafolio.js
@@ -0,0 +1,32 @@
+$(document).ready(function()
+{
+  new Clipboard('.btn-clipboard');
+
+  var scroll = getURLParameter("scroll");
+  if(scroll)
+  {
+    var url = window.location.href;
+    url = url.replace(/scroll=\d*&?/, '');
+    url = url.replace(/&$/, '');
+    url = url.replace(/\/\?$/, '/');
+
+    window.history.pushState("string", "Title", url);
+
+    $(window).scrollTop(scroll);
+  }
+
+});
+
+function getURLParameter(sParam)
+{
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+
+  for (var i = 0; i < sURLVariables.length; i++)
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam)
+      return sParameterName[1];
+  }
+  return null;
+}
